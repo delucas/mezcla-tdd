51725a6a67e861d4cb945f6ffe55a429e72d8b74
Lucas Videla <videla.lucas@gmail.com>
2013-06-15 13:37:26 -0300

    #GREEN: queSePuedenUnirDosArreglosDesiguales

--- src/Mezclador.java
+++ src/Mezclador.java
@@ -2,7 +2,18 @@
 public class Mezclador {
 
 	public int[] mezclar(int[] a, int[] b) {
-		int[] c = {1, 2};
+		int[] c = null;
+		if (a.length > 0 && b.length > 0) {
+			
+			c = new int[a.length + b.length];
+			
+			for (int i = 0; i < a.length; i++) {
+				c[i] = a[i];
+			}
+			for (int i = 0; i < b.length; i++) {
+				c[a.length + i] = b[i];
+			}
+		}
 		if (b.length == 0) {
 			c = a;
 		}
