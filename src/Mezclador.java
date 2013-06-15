f6bbdfd5dfcd365c1665dd34abfcdd2d1a58bcd3
Lucas Videla <videla.lucas@gmail.com>
2013-06-15 13:29:05 -0300

    #GREEN: queSePuedenUnirDosArreglos

--- src/Mezclador.java
+++ src/Mezclador.java
@@ -2,9 +2,12 @@
 public class Mezclador {
 
 	public int[] mezclar(int[] a, int[] b) {
-		int[] c = b;
+		int[] c = {1, 2};
 		if (b.length == 0) {
 			c = a;
+		}
+		if (a.length == 0) {
+			c = b;
 		}
 		return c;
 	}
