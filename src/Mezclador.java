7f1a6d5f1a301aee6abf8bd11f5eb6e09787f72e
Lucas Videla <videla.lucas@gmail.com>
2013-06-16 20:36:36 -0300

    #BLUE: Se extrae inicialización del caso más común

--- src/Mezclador.java
+++ src/Mezclador.java
@@ -2,10 +2,8 @@
 public class Mezclador {
 
 	public int[] mezclar(int[] a, int[] b) {
-		int[] c = null;
+		int[] c = new int[a.length + b.length];
 		if (a.length > 0 && b.length > 0) {
-			
-			c = new int[a.length + b.length];
 			
 			for (int i = 0; i < a.length; i++) {
 				c[i] = a[i];
